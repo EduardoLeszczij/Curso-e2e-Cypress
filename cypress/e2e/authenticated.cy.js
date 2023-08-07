1f1c0d66f6f59b0d603c3afca4c4886dbb3008cc
Eduardo Leszczij <eduardo.leszczij@grupobarigui.com.br>
2023-08-07 14:17:24 -0300

    Ajustes feitos no arquivo de atenticação

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -1,52 +1,49 @@
 // cypress/e2e/authenticated.cy.js
 
-import { faker } from '@faker-js/faker/locale/en';
+import { faker } from '@faker-js/faker/locale/en'
 
 describe('Scenarios where authentication is a pre-condition', () => {
   beforeEach(() => {
-    cy.intercept('GET', '**/notes').as('getNotes');
-    cy.sessionLogin();
-  });
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.sessionLogin()
+  })
 
   it('CRUDs a note', () => {
-    const noteDescription = faker.lorem.words(4);
+    const noteDescription = faker.lorem.words(4)
 
-    cy.createNote(noteDescription);
-    cy.wait('@getNotes');
+    cy.createNote(noteDescription)
+    cy.wait('@getNotes')
 
-    const updatedNoteDescription = faker.lorem.words(4);
-    const attachFile = true;
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
 
-    cy.editNote(noteDescription, updatedNoteDescription, attachFile);
-    cy.wait('@getNotes');
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait('@getNotes')
 
-    cy.deleteNote(updatedNoteDescription);
-    cy.wait('@getNotes');
-  });
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNotes')
+  })
 
   it('successfully submits the settings form', () => {
-    cy.intercept('POST', '**/prod/billing').as('paymentRequest');
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
 
-    cy.fillSettingsFormAndSubmit();
+    cy.fillSettingsFormAndSubmit()
 
-    cy.wait('@getNotes');
+    cy.wait('@getNotes')
     cy.wait('@paymentRequest')
       .its('state')
-      .should('be.equal', 'Complete');
-  });
+      .should('be.equal', 'Complete')
+  })
 
   it('logs out', () => {
     cy.visit('/')
-    cy.wait('@getNotes');
-    
+    cy.wait('@getNotes')
     if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
       cy.get('.navbar-toggle.collapsed')
         .should('be.visible')
-        .click();
-    };
-    
-    cy.contains('.nav a', 'Logout').click();
-
-    cy.get('#email').should('be.visible');
-  });
-});
+        .click()
+    }
+    cy.contains('.nav a', 'Logout').click()
+    cy.get('#email').should('be.visible')
+  })
+})
