f0c2f3d49e8006aaf5474f70fc68c22e15091ad2
Eduardo Leszczij <eduardo.leszczij@grupobarigui.com.br>
2023-08-07 17:24:17 -0300

    Configure ESlintrd

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-	chromeWebSecurity: false,
-	e2e: {
-		baseUrl: 'https://notes-serverless-app.com',
-		defaultCommandTimeout: 15000,
-		env: {
-			viewportWidthBreakpoint: 768,
-		},
-	},
-	projectId: 'ybquv7'
+  chromeWebSecurity: false,
+  e2e: {
+    baseUrl: 'https://notes-serverless-app.com',
+    defaultCommandTimeout: 15000,
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+  },
+  projectId: 'ybquv7'
 })
